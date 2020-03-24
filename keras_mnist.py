35ce7b715f9b3793a0d755521fb58e1a6666d26b
estromenko <48186405+estromenko@users.noreply.github.com>
2020-03-24 12:20:00 +0300

    Update keras_mnist.py

--- keras_mnist.py
+++ keras_mnist.py
@@ -4,8 +4,10 @@ from keras.datasets import mnist
 from keras import optimizers
 from keras import losses
 from keras.utils import to_categorical
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import os
+
 
 # Data initialization
 (train_data, train_labels), (test_data, test_labels) = mnist.load_data('data')
@@ -28,9 +30,27 @@ loss = losses.categorical_crossentropy
 metrics = ['accuracy']
 
 network.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-history = network.fit(train_data, train_labels, epochs=20, batch_size=128)
+history = network.fit(train_data, train_labels, epochs=5, batch_size=128)
 
 # Testing
 _, accuracy = network.evaluate(test_data, test_labels)
 
-print('\n', round(accuracy * 100, 2), '%')
+clear = lambda: os.system('clear')
+
+while True:
+    clear()
+    print('\n', round(accuracy * 100, 2), '%')
+    print('There are 10000 handwritten numbers from MNIST dataset.')
+    print('Neural network cat predict any of this numbers.')
+
+    try:
+        index = int(input('Type any index: '))
+    except:
+        index = int(input('Type any INTEGER index: '))
+
+    print('Neural network thinks its', end=' ')
+    print(np.argmax(network.predict(test_data)[index]))
+
+    plt.imshow(test_data[index].reshape((28, 28)))
+    plt.show()
+    input('Press Enter to continue: ')
